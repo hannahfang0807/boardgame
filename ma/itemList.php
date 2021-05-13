5d909c3a191a5631e76002757d4aafad719b3bfe
hannahfang0807 <doremi4002@gmail.com>
2021-05-13 17:24:17 +0800

    馬的

--- ma/itemList.php
+++ ma/itemList.php
@@ -8,7 +8,7 @@ require_once("./tpl/func-buildTree.php");
 require_once("./tpl/func-getRecursiveCategoryIds.php");
 ?>
 
-<div class="container">
+<div class="container mt-5">
     <div class="row">
         <!-- 樹狀商品種類連結 -->
         <div class="col-md-2 col-sm-3"><?php buildTree($pdo, 0); ?></div>
