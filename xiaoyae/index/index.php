0f78897aa6971433bbe0a1d9725e1961333ba00f
xiaoyae <zxc7536twtw@gmail.com>
2021-05-13 12:08:52 +0800

    05/13 update
    修改路徑 !!

--- xiaoyae/index/index.php
+++ xiaoyae/index/index.php
@@ -1,9 +1,9 @@
 <?php 
 session_start();
 require_once './db.inc.php';
-require_once '../../templates/html-header.php';
-require_once '../../templates/nav.php';
+require_once './templates/html-header.php';
+require_once '../../nav.php';
 require_once './store/admin/templates/tpl-product-list.php';
 // require_once './store/admin/templates/tpl-footer.php';
-require_once '../../templates/html-footer.php';
+require_once './templates/html-footer.php';
 ?>
