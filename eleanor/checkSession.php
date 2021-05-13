97341124b9665c0d300f5da693e495c3d09d83e6
JCEleanor <crcky4826@gmail.com>
2021-05-13 10:53:12 +0800

    更改

--- eleanor/checkSession.php
+++ eleanor/checkSession.php
@@ -2,7 +2,7 @@
 session_start();
 
 //判斷是否登入 (確認先前指派的 session 索引是否存在)
-if( !isset($_SESSION['username']) ) {
+if( !isset($_SESSION['memberAccount']) ) {
     //關閉 session
     session_destroy();
     
