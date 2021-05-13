a25310509248897bfb429d8de281557291bbab5d
hannahfang0807 <doremi4002@gmail.com>
2021-05-13 16:32:26 +0800

    調整Hannah檔案

--- hannah/title.php
+++ hannah/title.php
@@ -3,11 +3,11 @@
             <a href="../admin/admin.php" class="text-dark">桌遊店後台-宇宙迷航</a>
         </h1>
         <div>
-            <a class="p-2 text-dark font-weight-bold" href="">遊戲類別管理</a>
-            <a class="p-2 text-dark font-weight-bold" href="">付款方式管理</a>
-            <a class="p-2 text-dark font-weight-bold" href="">商品管理</a>
-            <a class="p-2 text-dark font-weight-bold" href="">新增商品</a>
-            <a class="p-2 text-dark font-weight-bold" href="">訂單一覽</a>
+            <a class="p-2 text-dark font-weight-bold" href="../admin/category.php">遊戲類別管理</a>
+            <a class="p-2 text-dark font-weight-bold" href="../admin/paymentType.php">付款方式管理</a>
+            <a class="p-2 text-dark font-weight-bold" href="#">商品管理</a>
+            <a class="p-2 text-dark font-weight-bold" href="#">新增商品</a>
+            <a class="p-2 text-dark font-weight-bold" href="../sophia/admin/orders.php">訂單一覽</a>
             <a class="p-2 text-dark font-weight-bold" href="./member.php">會員一覽</a>
             <!-- <a class="p-2 text-light font-weight-bold" href="">商品追蹤管理</a> -->
             <a class="p-2 text-dark font-weight-bold" href="../index.php">網站前台</a>
@@ -16,7 +16,7 @@
         <div>
            
                 <span class="text-dark">管理員 您好</span>
-                <a class="btn btn-dark my-0 ml-2" href="adminLogout.php">登出</a>
+                <a class="btn btn-dark my-0 ml-2" href="../admin/adminLogout.php">登出</a>
 
         </div>
     </nav>
