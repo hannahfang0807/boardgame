39100421c3934f890d57ca77ebb552b56ff9de34
Opener123 <bd0994024@gmail.com>
2021-05-13 17:11:27 +0800

    Create multiple_images.sql

---
+++ sql/multiple_images.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-05-13 11:11:00
+-- 伺服器版本： 10.4.18-MariaDB
+-- PHP 版本： 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `boardgame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `multiple_images`
+--
+
+CREATE TABLE `multiple_images` (
+  `multipleImageId` int(11) NOT NULL COMMENT '流水號',
+  `multipleImageImg` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '圖片名稱',
+  `itemId` int(11) NOT NULL COMMENT '商品編號',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '新增時間',
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新時間'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='商品圖片';
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `multiple_images`
+--
+ALTER TABLE `multiple_images`
+  ADD PRIMARY KEY (`multipleImageId`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `multiple_images`
+--
+ALTER TABLE `multiple_images`
+  MODIFY `multipleImageId` int(11) NOT NULL AUTO_INCREMENT COMMENT '流水號';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
