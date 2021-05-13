b4379fcd4032dbee9fb698bbff48c5e60b5e565e
yunlee0302 <a10038532@gmail.com>
2021-05-13 14:02:20 +0800

    購物車

---
+++ sophia/deleteCart.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once('./db.inc.php');
+require_once('./html-header.php'); 
+require_once('./nav.php'); 
+
+$objResponse['success'] = false;
+$objResponse['info'] = "查無購物車編號";
+
+header("Refresh: 3; url=./myCart.php");
+
+if( !isset($_GET["idx"]) ){
+    echo json_encode($objResponse, JSON_UNESCAPED_UNICODE);
+    exit();
+}
+
+//刪除指定的索引位置
+unset($_SESSION['cart'][$_GET["idx"]]);
+
+//重建索引
+$_SESSION['cart'] = array_values($_SESSION['cart']);
+
+$objResponse['success'] = true;
+$objResponse['info'] = "已刪除特定購物車商品項目";
+echo json_encode($objResponse, JSON_UNESCAPED_UNICODE);
+exit();
+
+require_once('./html-footer.php');
