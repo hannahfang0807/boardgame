c9586ad5a8dee6885999d663c910ce54f5b4948e
hannahfang0807 <doremi4002@gmail.com>
2021-05-13 09:22:46 +0800

    admin

---
+++ admin/templates/title.php
@@ -0,0 +1,1 @@
+後端管理頁 | <a href="./category.php">編輯類別</a> | <a href="./paymentType.php">編輯付款方式</a> | <a href="./admin.php">商品列表</a> | <a href="./new.php">新增商品</a> | <a href="./orders.php">訂單一覽</a> | <a href="./memberList.php">會員一覽</a> | <a href="./itemTracking.php">商品追蹤管理</a>| <a href="../index.php">網站前台</a> | <a href="../logout.php?logout=1">登出</a>
--- templates/nav.php
+++ templates/nav.php
@@ -3,11 +3,14 @@
             <a href="./index.php" class="text-dark">會員系統</a>
         </h1>
         <div>
-            <a class="p-2 text-dark" href="./itemList.php">商品一覽</a>
-            <a class="p-2 text-dark font-weight-bold" href="./admin/admin.php">管理員後台</a> 
+            <a class="p-2 text-dark font-weight-bold" href="./ma/itemList.php">商品一覽</a>
+            <a class="p-2 text-dark font-weight-bold" href="./eleanor/reservation.php">預約</a>
+            <a class="p-2 text-dark font-weight-bold" href="./board.php">揪團留言板</a>
+            <a class="p-2 text-dark font-weight-bold" href="./xiaoyae/index.php">分店資訊</a>
+            <a class="p-2 text-dark font-weight-bold" href="./yo/board/homepage.php">管理員後台</a> 
         </div>
         <div>
-            <a class="p-2 text-dark" href="./myCart.php">
+            <a class="p-2 text-dark" href="./ma/myCart.php">
                 <span>我的購物車</span>
                 (<span id="cartItemNum">
                 <?php 
@@ -20,10 +23,10 @@
                 </span>)
             </a>
             <?php if(isset($_SESSION["memberAccount"])) { ?>
-                <a class="p-2 text-dark" href="./member.php">會員中心</a>
+                <!-- <a class="p-2 text-dark" href="./hannah/member.php">會員中心</a> -->
             <?php } ?>
             <?php if(!isset($_SESSION["memberAccount"])){ ?>
-                <a class="btn btn-outline-success my-0" href="./register.php">註冊</a>
+                <a class="btn btn-outline-success my-0" href="./hannah/register.php">註冊</a>
                 <a class="btn btn-outline-success my-0" href="./login.php">登入</a>
             <?php } else { ?>
                 <span><?php echo $_SESSION["memberAccount"] ?> 您好</span>    
