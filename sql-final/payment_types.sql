3a7a6422e58a1316b1a3dd4611dd9b92d9e1ee7e
yunlee0302 <a10038532@gmail.com>
2021-05-14 11:53:50 +0800

    sql

---
+++ sql-final/payment_types.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-05-14 11:52:07
+-- 伺服器版本： 10.4.18-MariaDB
+-- PHP 版本： 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `boardgame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `payment_types`
+--
+
+CREATE TABLE `payment_types` (
+  `paymentTypeId` int(11) NOT NULL COMMENT '流水號',
+  `paymentTypeName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '付款方式名稱',
+  `paymentTypeImg` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '付款方式圖片名稱',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '新增時間',
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新時間'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='付款方式';
+
+--
+-- 傾印資料表的資料 `payment_types`
+--
+
+INSERT INTO `payment_types` (`paymentTypeId`, `paymentTypeName`, `paymentTypeImg`, `created_at`, `updated_at`) VALUES
+(2, 'Line pay', 'payment_type_20210513144250.jpg', '2021-04-23 14:38:58', '2021-05-13 14:42:50'),
+(3, '超商付款', 'payment_type_20210513145223.png', '2021-05-13 09:49:06', '2021-05-13 14:52:23'),
+(4, 'apple ', 'payment_type_20210513150230.png', '2021-05-13 15:02:30', '2021-05-14 11:18:54'),
+(5, 'ATM匯款', 'payment_type_20210513150339.png', '2021-05-13 15:03:39', '2021-05-13 16:50:59');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `payment_types`
+--
+ALTER TABLE `payment_types`
+  ADD PRIMARY KEY (`paymentTypeId`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `payment_types`
+--
+ALTER TABLE `payment_types`
+  MODIFY `paymentTypeId` int(11) NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
