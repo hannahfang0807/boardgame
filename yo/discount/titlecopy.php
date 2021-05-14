c00a73ac8d3044ede05cd0d250750c87aad6e8a2
Ryo1026 <c9pcv5nvbp@gmail.com>
2021-05-14 11:05:57 +0800

    5/14 nav連結整合完成
    後臺nav連結完成

---
+++ yo/discount/titlecopy.php
@@ -0,0 +1,22 @@
+<nav class="d-flex align-items-center p-3 px-4 navbar navbar-light bg-light border-bottom shadow-sm">
+    <h1 class="navbar-brand font-weight-bold my-0">
+        <a href="../admin/admin.php" class="text-dark">桌遊店後台-宇宙迷航</a>
+    </h1>
+    <div>
+        <a class="p-2 text-dark font-weight-bold" href="../../admin/category.php">遊戲類別管理</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../admin/paymentType.php">付款方式管理</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../admin/admin.php">商品管理</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../admin/new.php">新增商品</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../sophia/admin/orders.php">訂單一覽</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../hannah/member.php">會員一覽</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../xiaoyae/index/store/admin/admin.php">分店一覽</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../yo/discount/homepage.php">折價券管理</a>
+        <a class="p-2 text-dark font-weight-bold" href="../../index.php">網站前台</a>
+    </div>
+    <div>
+
+        <span class="text-dark">管理員 您好</span>
+        <a class="btn btn-dark my-0 ml-2" href="../admin/adminLogout.php">登出</a>
+
+    </div>
+</nav>
