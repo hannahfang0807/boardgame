36748e82c7d4bcc24d0d425c6c7a4934158bcb23
Ryo1026 <c9pcv5nvbp@gmail.com>
2021-05-14 11:50:19 +0800

    change SQL

---
+++ sql-final/reply.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-05-14 11:49:20
+-- 伺服器版本： 10.4.18-MariaDB
+-- PHP 版本： 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `boardgame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `reply`
+--
+
+CREATE TABLE `reply` (
+  `replyId` int(11) NOT NULL COMMENT '回文編號',
+  `messageId` int(11) NOT NULL COMMENT '留言編號',
+  `memberId` int(11) NOT NULL COMMENT '會員編號',
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '留言內容',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '新增時間',
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新時間'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `reply`
+--
+
+INSERT INTO `reply` (`replyId`, `messageId`, `memberId`, `content`, `created_at`, `updated_at`) VALUES
+(1, 1, 4, '我要+1', '2021-05-11 13:48:14', '2021-05-12 10:09:26'),
+(8, 2, 1, '87', '2021-05-12 15:22:39', '2021-05-12 15:22:39'),
+(13, 11, 7, '可憐哪', '2021-05-14 10:04:58', '2021-05-14 10:04:58');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`replyId`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `replyId` int(11) NOT NULL AUTO_INCREMENT COMMENT '回文編號', AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
